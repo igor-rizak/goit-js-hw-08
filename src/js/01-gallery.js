8d2881d7fb663984da00ed3446a37085af5c1185
igor-rizak <igor.rizak@ukr.net>
2023-05-01 11:05:01 +0300

    11

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,5 @@
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items';
+import { galleryItems } from '../gallery-items';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const galleryList = document.querySelector('.gallery');
