74327a4ebeafadb25b6c68d71c818aec84c849a1
igor-rizak <igor.rizak@ukr.net>
2023-05-01 12:57:39 +0300

    comit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,27 +1,28 @@
+// Change code below this line
+
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from '../gallery-items';
+import { galleryItems } from './gallery-items';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const galleryList = document.querySelector('.gallery');
+const gallerylink = document.querySelector('.gallery');
+const markupGallery = galleryItemsMarkup(galleryItems);
 
-
-
-const markupGallery = galleryItems.map(({preview, original, description}) => `<li class="gallery__item">
-  <a class="gallery__link" href="large-image.jpg">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>`).join('');
-
-galleryList.insertAdjacentHTML("afterbegin", markupGallery);
+gallerylink.insertAdjacentHTML("afterbegin", markupGallery);
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
 
-console.log(lightbox)
+
+function galleryItemsMarkup(items) {
+  return items.map(({ preview, original, description } = items) => 
+    `<li ><a class="gallery__item" 
+    href="${original}">
+    <img class="gallery__image"
+    src="${preview}"
+    alt="${description}" /></a></li>`
+  ).join('');
+};
+
+console.log(lightbox);
