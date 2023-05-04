bd5277112eeffbfe6957f240036576390407ee67
igor-rizak <igor.rizak@ukr.net>
2023-05-04 10:55:11 +0300

    comit hw 08

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,8 +5,8 @@ const formData = {};
 
 const refs = {
   form: document.querySelector('.feedback-form'),
-  inputEmail: document.querySelector('input[name="email"]'),
-  inputTextarea: document.querySelector('textarea[name="message"]'),
+  email: document.querySelector('input[name="email"]'),
+  textarea: document.querySelector('textarea[name="message"]'),
 };
 
 refs.form.addEventListener('input', throttle(onFormInput, 500));
@@ -17,20 +17,18 @@ getItems();
 function onFormSubmit(e) {
   e.preventDefault();
   
-  if (refs.inputEmail.value !== ''
-    && refs.inputTextarea.value !== '') {
+  if (refs.email.value !== '' && refs.textarea.value !== '') {
     localStorage.removeItem(LOCAL_STORAGE_KEY);
     e.currentTarget.reset();
     console.log(formData);
     return;
   }
-
     alert('Необхідно заповнити всі поля');  
 }
 
 function onFormInput() {
-  formData.email = refs.inputEmail.value;
-  formData.message = refs.inputTextarea.value;
+  formData.email = refs.email.value;
+  formData.message = refs.textarea.value;
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(formData));
 }
 
@@ -38,8 +36,8 @@ function getItems() {
   try {
     const saveItems = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
     if (saveItems) {
-      refs.inputEmail.value = saveItems.email;
-      refs.inputTextarea.value = saveItems.message;
+      refs.email.value = saveItems.email;
+      refs.textarea.value = saveItems.message;
     }
   } catch (e) {
     console.log(e.name); 
